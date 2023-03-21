54dec185bf4c9d9473dec4da0c589c27119936ed
Tomas <tomas.mangano.dev@gmail.com>
2023-03-21 15:05:50 -0300

    front end

---
+++ frontEnd/js/calendar.js
@@ -0,0 +1,16 @@
+// const dayjs = require('dayjs')
+
+import {weeklyCalendar} from './testing.js';  
+
+
+
+
+
+let calendar;
+
+
+// dayjs().format()
+ 
+
+
+weeklyCalendar(7,12, 2);
---
+++ frontEnd/js/testing.js
@@ -0,0 +1,50 @@
+
+const days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+const calendar = document.getElementById('calendar');
+
+function weeklyCalendarDraw(calendarCol, calendarTable){
+    for (let i = 0; i < calendarCol; i++) {
+        let calendarRow = document.createElement('tr');
+        calendarRow.classList.add('calRow');
+        calendarTable.appendChild(calendarRow);
+
+        for (let j = 0; j < 7; j++) {
+            let calendarCell = document.createElement('td');
+            calendarCell.classList.add('calCell');
+            calendarRow.appendChild(calendarCell);
+            if (i === 0) calendarCell.textContent = days[j];
+        }
+    }
+}
+function weeklyCalendarHourDraw(calendarCol, calendarTable){
+    for (let i = 0; i < calendarCol; i++) {
+        let calendarRow = document.createElement('tr');
+        calendarRow.classList.add('calRow');
+        calendarTable.appendChild(calendarRow);
+
+        let calendarCell = document.createElement('td');
+        calendarCell.classList.add('hourCell');
+        calendarRow.appendChild(calendarCell);
+    }
+}
+
+
+
+export function weeklyCalendar( initialHour, lastHour, durationTurns ) {
+    let calendarColumns = (lastHour - initialHour) * durationTurns + 1;
+    let calendarTable = document.createElement('table');
+    let hourTable = document.createElement('table');
+    calendarTable.id = 'calTable';
+    hourTable.id = 'hourTable';
+    calendar.appendChild(hourTable);
+    calendar.appendChild(calendarTable);
+
+    weeklyCalendarHourDraw(calendarColumns,hourTable);
+    weeklyCalendarDraw(calendarColumns, calendarTable);
+
+
+    
+}
+
+
+//module.exports = weeklyCalendar;
